f1ce7bc93803c84e64a85ead7c67b9a906cb48d7
Patrick Stoeckle <patrick.stoeckle@tum.de>
2020-10-20 22:19:28 +0200

    :sparkles: Base URL feature

--- src/gl_pages_forward/main.py
+++ src/gl_pages_forward/main.py
@@ -16,18 +16,21 @@ _LOGGER = getLogger(__name__)
 _ENV = Environment(
     loader=PackageLoader("gl_pages_forward", "rsc"), trim_blocks=True, autoescape=True
 )
+_INDEX_TEMPLATE = _ENV.get_template("index.html.j2")
+_OVERVIEW_TEMPLATE = _ENV.get_template("overview.html.j2")
 
 
+@option("--base_url", "-u", default="")
 @option("--output", "-o", default=join(".", "public"))
 @option("--config_file", "-c", default="config.yml")
 @command()
-def create_html_pages(config_file: str, output: str) -> None:
+def create_html_pages(config_file: str, output: str, base_url: str) -> None:
     """
 
     Args:
         config_file:
         output:
-
+        base_url:
     Returns:
 
     """
@@ -36,7 +39,7 @@ def create_html_pages(config_file: str, output: str) -> None:
             configuration: Dict[str, str] = safe_load(f_read.read())
         if configuration is None or not isinstance(configuration, dict):
             _LOGGER.critical(f"Please provide a valid YAML file in {config_file}")
-        template = _ENV.get_template("index.html.j2")
+
         if not isdir(output):
             mkdir(output)
         for file_name, url in configuration.items():
@@ -47,9 +50,14 @@ def create_html_pages(config_file: str, output: str) -> None:
                 mkdir(new_folder)
                 new_file = join(new_folder, "index.html")
             _LOGGER.info(f"Create file {new_file}")
-            content = template.render(new_url=url)
+            content = _INDEX_TEMPLATE.render(new_url=url)
             with open(new_file, "w") as f_write:
                 f_write.write(content)
+        if base_url != "":
+            with open(join(output, "overview.html"), "w") as f_write:
+                f_write.write(
+                    _OVERVIEW_TEMPLATE.render(url=base_url, websites=configuration.items())
+                )
     else:
         _LOGGER.critical(f"{config_file} is not a file!")
 
